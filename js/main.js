6491e6aa1770e96f17bf5fd6a445ca5af11f87ec
igor-shatrov <56680830+igor-shatrov@users.noreply.github.com>
2020-10-22 15:38:05 +0300

    feat: create first task

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 const time = document.getElementById('time');
 const greeting = document.getElementById('greeting');
+const name = document.getElementById('name');
+const focus = document.getElementById('focus')
+const date = document.getElementById('date')
+const monthes = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
+const daysOfWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
 
 function showTime() {
     let now = new Date();
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
-    let amPm = hours < 12 ? 'AM' : 'PM'
+    let dayOfWeek = now.getDay();
+    let day = now.getDate();
+    let mounth = now.getMonth();
 
 
+    date.innerText = `${daysOfWeek[dayOfWeek]}, ${addZero(day)} ${monthes[mounth]}`;
 
-    hours = hours < 12 ? hours : hours - 12
-
-    time.innerText = `${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)} ${amPm}`;
+    time.innerText = `${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`;
 }
 
 function addZero(number) {
@@ -34,19 +40,59 @@ function setBgGreet() {
     let hours = now.getHours();
 
     if (hours < 6) {
-        greeting.innerText = "Good Night";
+        greeting.innerText = "Доброй ночи";
         document.body.style.background = `URL('assets/images/night/${addZero(getRandomInt())}.jpg')`
+        c
     } else if (hours < 12) {
-        greeting.innerText = "Good Morning";
+        greeting.innerText = "Доброго утра";
         document.body.style.background = `URL('assets/images/morning/${addZero(getRandomInt())}.jpg')`
     } else if (hours < 18) {
-        greeting.innerText = "Good Afternoon";
+        greeting.innerText = "Доброго дня";
         document.body.style.background = `URL('assets/images/day/${addZero(getRandomInt())}.jpg')`
+        document.body.style.color = 'white'
     } else {
-        greeting.innerText = "Good Evening";
+        greeting.innerText = "Хорошего Вечера";
         document.body.style.background = `URL('assets/images/evening/${addZero(getRandomInt())}.jpg')`
+        document.body.style.color = 'white'
     }
-    setTimeout(setBgGreet(), 60000)
 }
 
 setBgGreet()
+
+function setInStorage(e) {
+    localStorage.setItem(e.target.id, e.target.innerText)
+}
+
+name.addEventListener('input', setInStorage)
+focus.addEventListener('input', setInStorage)
+name.addEventListener('keypress', pressEnter)
+focus.addEventListener('keypress', pressEnter)
+name.addEventListener('focus', clearOnFocus)
+focus.addEventListener('focus', clearOnFocus)
+
+
+function clearOnFocus(e){
+localStorage.setItem('last text',e.target.innerText)
+e.target.innerText=' '
+e.target.focus()
+}
+
+function pressEnter(e) {
+    if (e.keyCode === 13) {
+        e.target.blur()
+    } else {
+        return true
+    }
+}
+
+function getIntroStorage(target) {
+    if (localStorage.getItem(target.id) !== null) {
+        target.innerText = localStorage.getItem(target.id)
+    } else {
+        target.innerText = `[Введите ${target.id}]`
+    }
+
+}
+
+getIntroStorage(name)
+getIntroStorage(focus)
